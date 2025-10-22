21c81aacd2e1eea244a6212edaed897a5cb16667
Igor Emanuel <igor17emanuel@gmail.com>
2025-10-22 18:48:17 -0300

    rename controller

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,18 @@
 <?php
 
 use App\Controllers\HomeController;
-use App\Controllers\AuthController;
+use App\Controllers\AuthenticationController;
 use App\Controllers\AdminController;
 use App\Middleware\Admin;
 use Core\Router\Route;
 use Core\Router\RouteWrapperMiddleware;
 
-Route::get('/register', [AuthController::class, 'showRegistrationForm']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::get('/register', [AuthenticationController::class, 'showRegistrationForm']);
+Route::post('/register', [AuthenticationController::class, 'register']);
 Route::get('/', [HomeController::class, 'index'])->name('root');
-Route::get('/login', [AuthController::class, 'showLoginForm']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/login', [AuthenticationController::class, 'showLoginForm']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::post('/logout', [AuthenticationController::class, 'logout']);
 
 
 $adminGroup = new RouteWrapperMiddleware('admin');
